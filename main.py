1f2765eb2ebef274d19a30cc13f85517e760a67e
rumesh <rumesh.m.s16@gmail.com>
2022-03-18 20:58:48 +0530

    added files

---
+++ main.py
@@ -0,0 +1,27 @@
+import logging
+import os
+
+from fastapi import FastAPI
+
+app = FastAPI()
+logger = logging.getLogger("uvicorn")
+
+
+def exec_cmd(command):
+    out = os.system(command)
+    logger.info(str(out))
+
+
+@app.post('/')
+def func():
+    logger.info("WebHook received")
+    exec_cmd("git pull")
+    exec_cmd("mvn clean install")
+    exec_cmd("killall java")
+    return {}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", debug=False, reload=False, host="0.0.0.0")
