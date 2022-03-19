7940bb6b994e03a20160879d176c8ab01312b759
rumesh <rumesh.m.s16@gmail.com>
2022-03-19 18:13:16 +0530

    fixed: webhook timeout

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ cd webhook-server
 bash install.sh
 ```
 
-Configure WebHooks for each server
+Configure WebHooks for each GCP VM
 ![Screenshot from 2022-03-18 21-44-00](https://user-images.githubusercontent.com/32504465/159041091-111d443b-987a-4f69-b9b3-4b8e0e136482.png)
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install python3-pip -y
 pip3 install -r requirements.txt
-sudo mv ./webhook.service /etc/systemd/system/webhook.service
+sudo cp ./webhook.service /etc/systemd/system/webhook.service
 sudo systemctl daemon-reload
 sudo systemctl start webhook.service
 sudo systemctl enable webhook.service
